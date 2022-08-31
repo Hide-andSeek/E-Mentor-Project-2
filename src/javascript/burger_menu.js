ef34d8ca5632ec1b3ab62f361e9ead66dd30a2c8
Jade-Banares <proj.jade.banares@gmail.com>
2022-08-31 15:53:20 +0800

    Adding burger menu for mobile device

---
+++ src/javascript/burger_menu.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
+
