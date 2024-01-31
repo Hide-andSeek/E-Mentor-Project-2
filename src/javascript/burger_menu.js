d5080d3e9be80d04fd44d40560957ed54944893d
じぇいど <jade.arinal.banares@gmail.com>
2024-01-31 13:47:56 +0800

    minor changes

--- src/javascript/burger_menu.js
+++ src/javascript/burger_menu.js
@@ -1,29 +1,30 @@
-function openNav() {
-    document.getElementById("mySidenav").style.display = "block";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
-}
+const openNavigation = document.querySelector("#openNav");
+const closeNavigation = document.querySelector("#closeNav");
 
-function closeNav() {
-    document.getElementById("mySidenav").style.display = "none";
-    document.body.style.backgroundColor = "white";
-}
+closeNavigation.addEventListener("click", function () {
+  document.getElementById("mySidenav").style.display = "none";
+  document.body.style.backgroundColor = "white";
+});
 
-
+openNavigation.addEventListener("click", function () {
+  document.getElementById("mySidenav").style.display = "block";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+});
 
 function reveal() {
-    let reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      let windowHeight = window.innerHeight;
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      let elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
+
+window.addEventListener("scroll", reveal);
